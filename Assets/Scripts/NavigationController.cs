2974e1e3985c2e5dc9b9bd95ec8ec16cc33e8335
bitrasiva <bitrasiva@gmail.com>
2026-05-10 16:05:15 +0530

    bgcolor shader modified and adjustment and improvemnt the shader and renamed the uigradient shader.

--- Assets/Scripts/NavigationController.cs
+++ Assets/Scripts/NavigationController.cs
@@ -4,10 +4,24 @@ using UnityEngine.SceneManagement;
 public class NavigationController : MonoBehaviour
 {
     
-    //  Load scene by name
+      //Load scene by name
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+      SceneManager.LoadScene(sceneName);
     }
+    /*   [SerializeField] private Animator animator;
+
+    public void OnCloseButtonClicked()
+    {
+        animator.SetTrigger("Close");
+    }
+
+
+// Method called through animation event when the close animation is completed
+
+    public void OnClosedAnimationCompleted()
+    {
+        gameObject.SetActive(false);
+    }*/
 
 }

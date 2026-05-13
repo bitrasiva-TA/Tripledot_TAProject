e74a2fad263468d782ae04045eb0fbd514deeed3
bitrasiva <bitrasiva@gmail.com>
2026-05-13 14:57:59 +0530

    cleanup and aligned in cameraresolutioncheck script

--- Assets/Scripts/CameraResolutionCheck.cs
+++ Assets/Scripts/CameraResolutionCheck.cs
@@ -2,74 +2,63 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-    public class CameraResolutionCheck : MonoBehaviour
+public class CameraResolutionCheck : MonoBehaviour
+{
+    [SerializeField] CanvasScaler scaler;
+    enum DeviceType
     {
-        [SerializeField] CanvasScaler scaler;
-
-        enum DeviceType
+        Phone = 2,
+        Tablet = 3
+    }
+    void Awake()
+    {
+        var device = GetDeviceType();
+        if (device == DeviceType.Tablet)
         {
-            Phone = 2,
-            Tablet = 3
+            scaler.matchWidthOrHeight = 1f;
         }
-
-        void Awake()
+        else
         {
-
-
-            var device = GetDeviceType();
-
-            if (device == DeviceType.Tablet)
-            {
-                scaler.matchWidthOrHeight = 1f;
-            }
-            else
-            {
-                scaler.matchWidthOrHeight = 0f;
-            }
-
+            scaler.matchWidthOrHeight = 0f;
         }
-
-        private float DeviceDiagonalSizeInInches()
-        {
-            float screenWidth = Screen.width / Screen.dpi;
-            float screenHeight = Screen.height / Screen.dpi;
-            float diagonalInches = Mathf.Sqrt(Mathf.Pow(screenWidth, 2) + Mathf.Pow(screenHeight, 2));
-
-            return diagonalInches;
-        }
-
-        DeviceType GetDeviceType()
-        {
+    }
+    private float DeviceDiagonalSizeInInches()
+    {
+        float screenWidth = Screen.width / Screen.dpi;
+        float screenHeight = Screen.height / Screen.dpi;
+        float diagonalInches = Mathf.Sqrt(Mathf.Pow(screenWidth, 2) + Mathf.Pow(screenHeight, 2));
+        return diagonalInches;
+    }
+    DeviceType GetDeviceType()
+    {
 #if UNITY_IOS
     bool deviceIsIpad = UnityEngine.iOS.Device.generation.ToString().Contains("iPad");
-            if (deviceIsIpad)
-            {
-                return DeviceType.Tablet;
-            }
- 
-            bool deviceIsIphone = UnityEngine.iOS.Device.generation.ToString().Contains("iPhone");
-            if (deviceIsIphone)
-            {
-                return DeviceType.Phone;
-            }
+        if (deviceIsIpad)
+        {
+            return DeviceType.Tablet;
+        }
+        bool deviceIsIphone = UnityEngine.iOS.Device.generation.ToString().Contains("iPhone");
+        if (deviceIsIphone)
+        {
+            return DeviceType.Phone;
+        }
 #endif
 
 #if !UNITY_EDITOR
-            float aspectRatio = Mathf.Max(Screen.width, Screen.height) / Mathf.Min(Screen.width, Screen.height);
-            bool isTablet = (DeviceDiagonalSizeInInches() > 6.5f && aspectRatio < 2f);
+        float aspectRatio = Mathf.Max(Screen.width, Screen.height) / Mathf.Min(Screen.width, Screen.height);
+        bool isTablet = (DeviceDiagonalSizeInInches() > 6.5f && aspectRatio < 2f);
 #else
-            UnityEditor.PlayModeWindow.GetRenderingResolution(out uint width, out uint height);
-            float aspectRatio = Mathf.Max(width, height) / Mathf.Min(width, height);
-            bool isTablet = (DeviceDiagonalSizeInInches() > 6.5f && aspectRatio < 2f);
+        UnityEditor.PlayModeWindow.GetRenderingResolution(out uint width, out uint height);
+        float aspectRatio = Mathf.Max(width, height) / Mathf.Min(width, height);
+        bool isTablet = (DeviceDiagonalSizeInInches() > 6.5f && aspectRatio < 2f);
 #endif
-
-            if (isTablet)
-            {
-                return DeviceType.Tablet;
-            }
-            else
-            {
-                return DeviceType.Phone;
-            }
+        if (isTablet)
+        {
+            return DeviceType.Tablet;
+        }
+        else
+        {
+            return DeviceType.Phone;
         }
     }
+}

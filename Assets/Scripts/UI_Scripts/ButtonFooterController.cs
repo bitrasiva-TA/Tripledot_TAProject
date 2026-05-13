4636ac045182d9257d479d5d8213300e668286ae
bitrasiva <bitrasiva@gmail.com>
2026-05-13 14:50:21 +0530

    cleanup alignement in the buttonfootercontroller script

--- Assets/Scripts/UI_Scripts/ButtonFooterController.cs
+++ Assets/Scripts/UI_Scripts/ButtonFooterController.cs
@@ -4,46 +4,40 @@ using UnityEngine.UI;
 
 
 
-    public class ButtonFooterController : MonoBehaviour
+public class ButtonFooterController : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private Button footerBtn;
+    [SerializeField] private bool lockOnAwake;
+
+    [Header("Events")]
+    public UnityEvent<ButtonFooterController> OnButtonClickedEvent;
+
+    //Internal
+    private bool selected;
+    private bool locked;
+    private void Awake()
     {
-        [Header("Components")]
-        [SerializeField] private Animator animator;
-        [SerializeField] private Button footerBtn;
-        [SerializeField] private bool lockOnAwake;
-
-        [Header("Events")]
-        public UnityEvent<ButtonFooterController> OnButtonClickedEvent;
-
-        //Internal
-        private bool selected;
-        private bool locked;
-        private void Awake()
-        {
-            SetLock(lockOnAwake);
-        }
-
-        private void Start()
-        {
-            footerBtn.onClick.AddListener(() =>
-            {
-                OnButtonClickedEvent?.Invoke(this);
-            });
-        }
-
-        public void SetLock(bool locked)
-        {
-            locked = locked;
-
-            footerBtn.interactable = locked == false;
-
-            animator.SetBool("Locked", locked);
-        }
-
-        public void SetSelect(bool selected)
-        {
-            selected = selected;
-
-            animator.SetBool("Selected", selected);
-        }
+        SetLock(lockOnAwake);
     }
+    private void Start()
+    {
+        footerBtn.onClick.AddListener(() =>
+        {
+            OnButtonClickedEvent?.Invoke(this);
+        });
+    }
+    public void SetLock(bool locked)
+    {
+        locked = locked;
+        footerBtn.interactable = locked == false;
+        animator.SetBool("Locked", locked);
+    }
+    public void SetSelect(bool selected)
+    {
+        selected = selected;
+        animator.SetBool("Selected", selected);
+    }
+}
 
